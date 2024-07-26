51509941db7f470ef721c1f36b78e389d891803a
victormuteithia <muteithiavictor@gmail.com>
2024-07-26 15:43:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+document.addEventListener("keydown", function(event){
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }else if(event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+    
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10);
+
+    if(right < 360){
+        dodger.style.left = `${right + 1}px`
+    }
+}
